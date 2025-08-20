382cb54a5580bde67e626df9bd6d0307ee6def10
RodolfoU18 <rodolfo.hernandezd@uanl.edu.mx>
2025-08-20 00:03:06 -0600

    Add files via upload

---
+++ Ejercicio_6_AuditoriaBasica/AuditoriaBasica.psm1
@@ -0,0 +1,33 @@
+﻿function Obtener-UsuariosInactivos {
+<#
+.SYNOPSIS
+Obtiene usuarios locales habilitados que nunca han iniciado sesión
+
+.Descripción
+Esta funcion busca cuentas locales habilitadas que no tienen fecha de ultimo inicio de sesión
+
+.Example
+Get-UnactiveUsers
+
+.Notes
+Puede ayudar a detectar cuentas incesesarias o riesgosas en auditorias básicas.
+#>
+    Get-LocalUser | Where-Object { $_.Enabled -eq $true -and -not $_.LastLogon}
+}
+
+function Obtener-ServiciosExternos {
+<#
+.SYNOPSIS
+Obtiene servicios en ejecución que no pertenecen explicitamente a Windows 
+
+.Descripción
+Filtra servicios activos cuyo nombre descriptivo no contiene el término "Windows"
+
+.Example
+Obtener-ServiciosExternos
+
+.Notes
+Util para detectar softwares de terceros en segundo plano
+#>
+    Get-Service | Where-Object { $_.Status -eq "Running" -and $_.DisplayName -notmatch "Windows"}
+}
---
+++ Ejercicio_6_AuditoriaBasica/Principal.ps1
@@ -0,0 +1,42 @@
+﻿Clear-Host
+Import-Module AuditoriaBasica
+
+Write-Host "===Auditoria basica de usuarios y servicios==="
+Write-Host "1.- Mostrar Usuarios Inactivos"
+Write-Host "2.- Mostrar Servicios externos Activos"
+
+$Opcion = Read-Host "Selecciona una opcion (1 o 2)"
+
+switch ($Opcion){
+    
+    "1" { 
+
+        $usuarios = Obtener-UsuariosInactivos 
+
+        $usuarios | Format-Table Name, Enabled, LastLogon -AutoSize 
+
+        $usuarios | Export-Csv -Path "$env:USERPROFILE\Desktop\users_inac.csv" -NoTypeInformation 
+
+        Write-Host "`n📄 Reporte generado: ussers_inac.csv" 
+
+    } 
+
+    "2" { 
+
+        $servicios = Obtener-ServiciosExternos 
+
+        $servicios | Format-Table DisplayName, Status, StartType -AutoSize 
+
+        $servicios | ConvertTo-Html | Out-File "$env:USERPROFILE\Desktop\serv_e.html" 
+
+        Write-Host "`n📄 Reporte generado: serv_e.html" 
+
+    } 
+
+    default { 
+
+        Write-Host "Opción no válida." 
+
+    } 
+
+}
